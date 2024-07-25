c608e87d025c43e3d587bd33ace65250ed0ed49c
surajkumarskc2 <155421908+surajkumarskc2@users.noreply.github.com>
2024-07-26 02:32:47 +0530

    First commit by suraj
    Added new sop

--- Demo.java
+++ Demo.java
@@ -2,6 +2,8 @@ class Test{
     public static void main(String args[]){
 
         System.out.Println("Hi  Mr.Rajesh !");
+         System.out.Println("Hi  Mr.Skc !");
+        System.out.Println("Successfully commited !");
         
         System.out.Println("git branch created succesfully traying 1st commit");
 
