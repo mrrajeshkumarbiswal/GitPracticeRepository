fd91211c47fc3c733375dd31608f39d98dc3fd94
Rajesh Kumar Biswal <126473015+mrrajeshkumarbiswal@users.noreply.github.com>
2024-07-26 00:40:57 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,9 +1,9 @@
 class Test{
     public static void main(String args[]){
-<<<<<<< HEAD
+
         System.out.Println("Hi  Mr.Rajesh !");
-=======
+        
         System.out.Println("Hi  Rajesh !!")
->>>>>>> 53abfbc4bc2f8548b08ff7fc1ab1c05f610d19c0
+
     }
 }
